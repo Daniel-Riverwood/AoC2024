82fb646f9a0e26282ccbf4d316e1ce477a9c633b
Francois Botha <Francois@synthesis.co.za>
2024-12-01 08:03:01 +0200

    convert to dictionary for the seek so it's faster

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -6,22 +6,40 @@ public class Day01 : BaseDay
     private readonly List<string> _input;
     private List<int> firstpositions = new List<int>();
     private List<int> lastpositions = new List<int>();
+    private Dictionary<int, int> occurenceList = new Dictionary<int, int>();
     public Day01()
     {
         _input = File.ReadAllText(InputFilePath).SplitByNewline();
     }
 
-    private string ProcessInput1(List<string> input)
+    private void GenerateCollection(List<string> input, bool isPartTwo = false)
     {
         firstpositions = new List<int>();
         lastpositions = new List<int>();
-        int sum = 0;
-        foreach(var line in input)
+        foreach (var line in input)
         {
-            var segment = line.Split(' ');
+            var segment = line.Split("   ");
+            var converted = Convert.ToInt32(segment[1].Trim());
             firstpositions.Add(Convert.ToInt32(segment[0].Trim()));
-            lastpositions.Add(Convert.ToInt32(segment[3].Trim()));
+            
+            if (isPartTwo)
+            {
+                if (occurenceList.ContainsKey(converted))
+                    occurenceList[converted] += 1;
+                else occurenceList.Add(converted, 1);
+            }
+            else
+            {
+                lastpositions.Add(converted);
+            }
         }
+    }
+
+    private string ProcessInput1(List<string> input)
+    {
+        occurenceList = new Dictionary<int, int>();
+        GenerateCollection(input);
+        int sum = 0;
 
         firstpositions.Sort();
         lastpositions.Sort();
@@ -36,20 +54,14 @@ public class Day01 : BaseDay
 
     private string ProcessInput2(List<string> input)
     {
-        firstpositions = new List<int>();
-        lastpositions = new List<int>();
+        GenerateCollection(input, true);
         int sum = 0;
-        foreach (var line in input)
-        {
-            var segment = line.Split(' ');
-            firstpositions.Add(Convert.ToInt32(segment[0].Trim()));
-            lastpositions.Add(Convert.ToInt32(segment[3].Trim()));
-        }
 
-
-        for (var i = 0; i < firstpositions.Count(); i++)
+        var len = firstpositions.Count;
+        for (var i = 0; i < len; i++)
         {
-            var appearances = lastpositions.Where(q => q == firstpositions[i]).Count();
+            var appearances = occurenceList.GetValueOrDefault(firstpositions[i]);
+            if (appearances == default) continue;
             sum += firstpositions[i] * appearances;
         }
 
