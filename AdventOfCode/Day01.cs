15029f6cc99c926722eee4d57947889f36fdcaeb
Francois Botha <Francois@synthesis.co.za>
2024-12-01 07:15:54 +0200

    Day1

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -1,15 +1,63 @@
-﻿namespace AdventOfCode;
+﻿using Utilities;
+namespace AdventOfCode;
 
 public class Day01 : BaseDay
 {
-    private readonly string _input;
-
+    private readonly List<string> _input;
+    private List<int> firstpositions = new List<int>();
+    private List<int> lastpositions = new List<int>();
     public Day01()
     {
-        _input = File.ReadAllText(InputFilePath);
+        _input = File.ReadAllText(InputFilePath).SplitByNewline();
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    private string ProcessInput1(List<string> input)
+    {
+        firstpositions = new List<int>();
+        lastpositions = new List<int>();
+        int sum = 0;
+        foreach(var line in input)
+        {
+            var segment = line.Split(' ');
+            firstpositions.Add(Convert.ToInt32(segment[0].Trim()));
+            lastpositions.Add(Convert.ToInt32(segment[3].Trim()));
+        }
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+        firstpositions.Sort();
+        lastpositions.Sort();
+
+        for(var i = 0; i < firstpositions.Count(); i++)
+        {
+            sum += Math.Abs(firstpositions[i] - lastpositions[i]);
+        }
+        
+        return $"{sum}";
+    }
+
+    private string ProcessInput2(List<string> input)
+    {
+        firstpositions = new List<int>();
+        lastpositions = new List<int>();
+        int sum = 0;
+        foreach (var line in input)
+        {
+            var segment = line.Split(' ');
+            firstpositions.Add(Convert.ToInt32(segment[0].Trim()));
+            lastpositions.Add(Convert.ToInt32(segment[3].Trim()));
+        }
+
+
+        for (var i = 0; i < firstpositions.Count(); i++)
+        {
+            var appearances = lastpositions.Where(q => q == firstpositions[i]).Count();
+            sum += firstpositions[i] * appearances;
+        }
+
+        return $"{sum}";
+    }
+
+
+    public override ValueTask<string> Solve_1() => new(ProcessInput1(_input));
+
+    public override ValueTask<string> Solve_2() => new(ProcessInput2(_input));
 }
